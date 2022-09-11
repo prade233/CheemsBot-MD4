89a5920bf5e15d41e6d95fec392e2795ce02e43e
prade233 <112797940+prade233@users.noreply.github.com>
2022-09-11 13:05:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -70,7 +70,7 @@ global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.author = "PRADEEP" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
